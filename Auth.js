4b8bd9c9d88ac84464e97288feacbd9de234181c
jaeby06 <pilapiljakebryan@gmail.com>
2024-10-29 19:15:16 +0800

    new

---
+++ Auth.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+
+const Auth = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = () => {
+    if (isSignIn) {
+      // Simulate sign-in
+      Alert.alert('Your in', `Email: ${email}, Password: ${password}`);
+    } else {
+      // Simulate sign-up
+      Alert.alert('Registered', `Email: ${email}, Password: ${password}`);
+    }
+    // Clear inputs
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{isSignIn ? 'Sign In' : 'Sign Up'}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <View style={styles.buttonContainer}>
+        <Button title={isSignIn ? 'Login' : 'Register'} onPress={handleSubmit} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          title={isSignIn ? 'Sign Up' : 'Sign In'}
+          onPress={() => setIsSignIn(!isSignIn)}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+  },
+  buttonContainer: {
+    marginVertical: 5, // Add vertical margin between buttons
+  },
+});
+
+export default Auth;
